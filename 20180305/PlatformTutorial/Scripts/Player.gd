f0918f60a7f4739893c44b140af9b260fcb03939
Craigory V Coppola <agentendercake@gmail.com>
2018-03-05 17:04:13 -0500

    Add files via upload

---
+++ 20180305/PlatformTutorial/Scripts/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const UPVECTOR = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMPHEIGHT = -500
+
+var motion = Vector2(0,0)
+
+func _physics_process(delta):
+	if(motion.x < 5):
+		motion.x = 0
+	else:
+		motion.x-=sign(motion.x)*75 #Friction
+	motion.y+=GRAVITY #Gravity
+	
+	if(Input.is_action_pressed("ui_right")):
+		motion.x = SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Run")
+	elif(Input.is_action_pressed("ui_left")):
+		motion.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Run")
+	elif(is_on_floor()):
+		$AnimatedSprite.play("Idle")
+	
+	if(not is_on_floor()):
+		$AnimatedSprite.play("Jump")
+	
+	if(is_on_floor() and Input.is_action_just_pressed("ui_up")):
+		motion.y = JUMPHEIGHT
+	
+	
+	motion = move_and_slide(motion, UPVECTOR)
+	pass
