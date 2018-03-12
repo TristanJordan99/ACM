bae5acea1a83c7a3af72c3d460343bc6f1df4183
Craigory V Coppola <agentendercake@gmail.com>
2018-03-12 17:15:22 -0400

    Add files via upload

---
+++ 20180312/PlatformTutorial/NextLevel.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+export var  nextLevel = "res://wolrd2.tscn"
+
+func _process(delta):
+	for body in get_overlapping_bodies():
+		if body.name == "Player":
+			get_tree().change_scene(nextLevel)
+	pass
---
+++ 20180312/PlatformTutorial/Scripts/Player.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+
+const UPVECTOR = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMPHEIGHT = -500
+
+var motion = Vector2(0,0)
+
+func _input(event):
+	if (event.is_action_pressed("ui_cancel") and not event.is_echo()):
+		get_tree().reload_current_scene()
+
+func _physics_process(delta):
+	if(motion.x < 5):
+		motion.x = 0
+	else:
+		motion.x-=sign(motion.x)*75 #Friction
+	motion.y+=GRAVITY #Gravity
+	
+	if(Input.is_action_pressed("ui_right")):
+		motion.x = SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Run")
+	elif(Input.is_action_pressed("ui_left")):
+		motion.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Run")
+	elif(is_on_floor()):
+		$AnimatedSprite.play("Idle")
+	
+	if(not is_on_floor()):
+		$AnimatedSprite.play("Jump")
+	
+	if(is_on_floor() and Input.is_action_just_pressed("ui_up")):
+		motion.y = JUMPHEIGHT
+		$JumpSound.play()
+	
+	if(position.y > 800):
+		get_tree().reload_current_scene()
+	
+	motion = move_and_slide(motion, UPVECTOR)
+	pass
+
+
+
+
+
+
+
+
